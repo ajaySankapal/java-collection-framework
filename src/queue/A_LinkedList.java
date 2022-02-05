35eb4dc90d266f8715e8db23c61d352f3af20e76
ajaySankapal <90617524+ajaySankapal@users.noreply.github.com>
2022-02-05 14:27:02 +0530

    Queue LinkedList

---
+++ src/queue/A_LinkedList.java
@@ -0,0 +1,39 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//queue interface is implementated by LinkedList
+
+public class A_LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//initialization//implementation of queue by LinkedList
+		Queue<Integer> queue = new LinkedList<>();
+		
+		//add elements in queue
+		queue.offer(12);
+		queue.offer(11);
+		queue.offer(10);
+		queue.offer(9);
+		System.out.println(queue);
+		
+		// clear whole queue
+//		queue.clear();
+//		System.out.println(queue);
+		
+		queue.add(8);
+		System.out.println(queue);
+		System.out.println(queue.contains(9));
+		  
+//      remove first element which is added
+	   System.out.println(queue.poll() );	
+	   System.out.println(queue);
+	   
+// 
+	   System.out.println(queue.peek());
+	}
+
+}
